d1d28e34c8b60412712e82050d4d5200fc0a07d8
ygthnkrsln <forbequem@gmail.com>
2022-11-17 12:28:28 +0300

    SalesTax was added to Lecture_Studies.Lecture2

---
+++ src/Lecture_Studies/Lecture2/SalesTax.java
@@ -0,0 +1,15 @@
+package Lecture_Studies.Lecture2;
+
+import java.util.Scanner;
+
+public class SalesTax {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter your purchase amount: ");
+        double purchaseAmount = input.nextDouble();
+
+        double tax = purchaseAmount * 0.06;
+        System.out.println("Sales tax is $" + (int) ((tax * 100) / 100.0));
+    }
+}
