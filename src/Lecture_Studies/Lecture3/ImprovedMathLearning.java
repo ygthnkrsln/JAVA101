32014c817f98378030ef52a26e653e901e9cb6c7
Yiğithan <forbequem@gmail.com>
2022-12-01 13:38:33 +0300

    ImprovedMathLearning was developed according to new knowledge.

--- src/Lecture_Studies/Lecture3/ImprovedMathLearning.java
+++ src/Lecture_Studies/Lecture3/ImprovedMathLearning.java
@@ -3,6 +3,34 @@ package Lecture_Studies.Lecture3;
 import java.util.Scanner;
 
 public class ImprovedMathLearning {
+    static Scanner input = new Scanner(System.in);
+
+    public static void add(int a, int b) {
+        System.out.printf("What is %d + %d ?\n", a, b);
+
+        while (true) {
+            int answerAdd = input.nextInt();
+
+            if (answerAdd == a + b) {
+                System.out.println("Correct!");
+                break;
+            } else System.out.println("False! Let's give another try.");
+        }
+    }
+
+    public static void sub(int a, int b) {
+        System.out.printf("What is %d - %d ?\n", a, b);
+
+        while (true) {
+            int answerSub = input.nextInt();
+
+            if (answerSub == a - b) {
+                System.out.println("Correct!");
+                break;
+            } else System.out.println("False! Let's give another try.");
+        }
+    }
+
     public static void main(String[] args) {
         int a = (int) (Math.random() * 100);
         int b = (int) (Math.random() * 100);
@@ -13,11 +41,11 @@ public class ImprovedMathLearning {
             b = temp;
         }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.printf("What is %d - %d ?\n", a, b);
-        int answer = scanner.nextInt();
+        System.out.println("1: Addition, 2: Subtraction" + "\nWhich type of question do you want?");
+        int answerType = input.nextInt();
 
-        if (answer == a - b) System.out.println("Correct!");
-        else System.out.println("False!");
+        if (answerType == 1) add(a, b);
+        else if (answerType == 2) sub(a, b);
+        else System.out.println("Please enter valid value. The value should be 1 or 2. ");
     }
 }
