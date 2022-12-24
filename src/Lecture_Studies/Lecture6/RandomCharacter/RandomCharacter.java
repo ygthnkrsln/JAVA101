738070b36a0ce2057330d28b86bd3e3544fc8dde
Yiğithan <forbequem@gmail.com>
2022-12-24 23:52:54 +0300

    RandomCharacter was modified back to allow reaching getRandom codes.

--- src/Lecture_Studies/Lecture6/RandomCharacter/RandomCharacter.java
+++ src/Lecture_Studies/Lecture6/RandomCharacter/RandomCharacter.java
@@ -1,23 +1,23 @@
 package Lecture_Studies.Lecture6.RandomCharacter;
 
 public class RandomCharacter {
-    static char getRandomCharacter(char ch1, char ch2) {
+    public static char getRandomCharacter(char ch1, char ch2) {
         return (char) (ch1 + Math.random() * (ch2 - ch1 + 1));
     }
 
-    static char getRandomLowerCaseLetter() {
+    public static char getRandomLowerCaseLetter() {
         return getRandomCharacter('a', 'z');
     }
 
-    static char getRandomUpperCaseLetter() {
+    public static char getRandomUpperCaseLetter() {
         return getRandomCharacter('A', 'Z');
     }
 
-    static char getRandomDigitCharacter() {
+    public static char getRandomDigitCharacter() {
         return getRandomCharacter('0', '9');
     }
 
-    static char getRandomCharacter() {
+    public static char getRandomCharacter() {
         return getRandomCharacter('\u0000', '\uFFFF');
     }
 }
