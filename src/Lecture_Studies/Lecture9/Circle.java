5c9d76ac3178882c4f5409952114eedd7dc26648
Yiğithan <forbequem@gmail.com>
2022-12-26 23:01:48 +0300

    Circle, CircleTest, Student, StudentTest and RandomWithSeed were committed.

---
+++ src/Lecture_Studies/Lecture9/Circle.java
@@ -0,0 +1,21 @@
+package Lecture_Studies.Lecture9;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    double getArea() {
+        return radius * radius * Math.PI;
+    }
+}
---
+++ src/Lecture_Studies/Lecture9/Student.java
@@ -0,0 +1,8 @@
+package Lecture_Studies.Lecture9;
+
+public class Student {
+    String name;
+    int age;
+    boolean isScienceMajor;
+    char gender;
+}
