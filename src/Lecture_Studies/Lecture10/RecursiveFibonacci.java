aef6e9aa86b736b4392d9bffca565558de46161f
Yiğithan <forbequem@gmail.com>
2022-12-30 09:18:52 +0300

    RecursiveFibonacci was debugged.

--- src/Lecture_Studies/Lecture10/RecursiveFibonacci.java
+++ src/Lecture_Studies/Lecture10/RecursiveFibonacci.java
@@ -2,7 +2,7 @@ package Lecture_Studies.Lecture10;
 
 public class RecursiveFibonacci {
     public static int fib(int n) {
-        if (n <= 1) return n;
+        if (n <= 1) return 1;
 
         return fib(n - 1) + fib(n - 2);
     }
