ba12c2e8dee4e56be4da7efb223e167590daa129
Yiğithan <forbequem@gmail.com>
2023-01-27 19:00:34 +0300

    Studies of Lecture09 were committed.

---
+++ src/Lecture_Studies/Lecture09/CircleWithStaticMembers.java
@@ -0,0 +1,24 @@
+package Lecture_Studies.Lecture09;
+
+public class CircleWithStaticMembers {
+    double radius;
+    static int numberOfObjects = 0;
+
+    CircleWithStaticMembers() {
+        radius = 1;
+        numberOfObjects++;
+    }
+
+    CircleWithStaticMembers(double newRadius) {
+        radius = newRadius;
+        numberOfObjects++;
+    }
+
+    static int getNumberOfObjects() {
+        return numberOfObjects;
+    }
+
+    double getArea() {
+        return radius * radius * Math.PI;
+    }
+}
